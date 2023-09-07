9d145a9a7d3050a78aa95dc8f3c06d69c75ddf8f
kstechly <kayastechly@gmail.com>
2023-09-07 10:24:56 -0700

    added chromatic number precompute script

--- llm-csp/domain_utils/graph_coloring.py
+++ llm-csp/domain_utils/graph_coloring.py
@@ -1,18 +1,22 @@
 DEFAULT_PROMPT_START = "Color the following graph, described as a set of edges, such that no two vertices on the same edge share a color.\n"
 DEFAULT_PROMPT_END = "Please provide each vertex's color. Each color must be provided on a new line in the response and should be formatted as \"{VERTEX NUMBER}: {VERTEX COLOR ASSIGNMENT}\". Please do not provide anything else in your response."
 
+CHROMATIC_NUMBER_KEY = "OPTIMAL CHROMATIC NUMBER === "
+GRAPH_COLORING_DIRECTORY = "../data/graph_coloring/"
+
 import  grinpy
+import os
 
 def parse_dimacs(instance_text):
     parsed = []
-    print(instance_text)
-    for line in instance_text.split("\n"):
-        print(line)
+    for line in str(instance_text).split("\n"):
         text = line.split(" ")
         if text[0] == "e":
             parsed.append((text[1],text[2]))
     return parsed
 def optimal_coloring_number(instance_text):
+    if CHROMATIC_NUMBER_KEY in instance_text:
+        return instance_text.split(CHROMATIC_NUMBER_KEY)[1].split("\n")[0]
     graph = grinpy.Graph(parse_dimacs(instance_text))
     return grinpy.chromatic_number(graph)
 def check_coloring(coloring, instance_text):
@@ -47,3 +51,20 @@ def evaluate(instance_text,model_response):
         color = assignment[1]
         coloring[vertex_number] = color
     return check_coloring(coloring, instance_text)
+
+
+#### Precompute script         
+
+if __name__ == "__main__":
+    print(f"Precomputing chromatic numbers for all instances in {GRAPH_COLORING_DIRECTORY}")
+    for instance in os.listdir(GRAPH_COLORING_DIRECTORY):
+        if instance.startswith("instance-"):
+            with open(GRAPH_COLORING_DIRECTORY+instance,"r+") as fp:
+                instance_text = fp.read()
+                print(instance_text)
+                if CHROMATIC_NUMBER_KEY in instance_text:
+                    print(f"Instance {instance} was already precomputed. Skipping.")
+                    continue
+                chromatic = optimal_coloring_number(instance_text)
+                print(f"Instance {instance}'s chromatic number is {chromatic}")
+                fp.write(f"\nc {CHROMATIC_NUMBER_KEY}{chromatic}")
