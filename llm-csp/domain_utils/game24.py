7d43e8d2695c59c1b5b9fb2a8feb3680b9ba287a
kstechly <kayastechly@gmail.com>
2023-11-02 13:32:17 -0700

    bug fixes and error handling

--- llm-csp/domain_utils/game24.py
+++ llm-csp/domain_utils/game24.py
@@ -7,7 +7,7 @@ CSV_LOCATION = "../data/game24/24.csv"
 GAME24_DIRECTORY = "../data/game24/"
 
 END_TAG = "[ANSWER END]"
-DEFAULT_PROMPT_START = f"Use numbers and basic arithmetic operations (+ - * /) to obtain 24. Write your answer first. At the end of your answer, write {END_TAG}\n"
+DEFAULT_PROMPT_START = f"Use numbers and basic arithmetic operations (+ - * /) to obtain 24. You must write your response Write your answer first. At the end of your answer, write {END_TAG}\n"
 # \nInput: 4 4 6 8\nAnswer: (4 + 8) * (6 - 4) = 24
 STOP_PHRASE = "Verifier confirmed success."
 
@@ -26,6 +26,7 @@ def check_answer(instance_text, equation):
         return sympy.simplify(expression) == 24, f"This expression evaluates to {sympy.simplify(expression)} instead of 24."
     except Exception as e:
         print(e)
+        print(expression)
         return False, "This expression is malformed."
 
 #### Required Functions
@@ -52,15 +53,28 @@ def evaluate(instance_text, response_trace, problem_type=""):
         last_backprompt = f"backprompt {len(backprompt_keys)-1}"
     evaluation["correct"],_ = check_answer(instance_text, response_trace[last_response])
     evaluation["number of backprompts before correct"] = 0
+    answers = []
     for x in response_keys:
         evaluation[x],_ = check_answer(instance_text,response_trace[x])
+        expression = response_trace[x].strip().split('\n')[-1].lower().replace('answer: ', '').split('=')[0]
+        answer = "malformed"
+        try:
+            answer = sympy.simplify(expression)
+        except Exception as e: 
+            print(e)
+        evaluation[f"{x} eval"] = str(answer)
+        answers.append(answer)
         if not evaluation["number of backprompts before correct"] and evaluation[x] and not x == "response":
             evaluation["number of backprompts before correct"] = int(x[-2:])
     evaluation["number of backprompts"] = len(response_keys)-1
+    evaluation["unique answers"] = len(list(set(answers)))
 
     evaluation["stopped"] = False
     if not last_backprompt or STOP_PHRASE.replace(".", "") in response_trace[last_backprompt]:
         evaluation["stopped"] = True
+
+    #TODO check how many different answers it has generated (basic version: eval each, check num unique final answers)
+
 
     evaluation["ever correct"] = False
     evaluation["total correct"] = 0
@@ -73,15 +87,9 @@ def evaluate(instance_text, response_trace, problem_type=""):
 
 def backprompt(instance_text, model_response, backprompt_type):
     check, reason = check_answer(instance_text, model_response)
-    if check:
-        return STOP_PHRASE
-    if backprompt_type == "passfail":
-        return f"Feedback: This is not correct. {DEFAULT_BACKPROMPT_END(instance_text)}"
-    elif backprompt_type == "first":
-        return f"Feedback: This is not correct. {reason} {DEFAULT_BACKPROMPT_END(instance_text)}"
-    elif backprompt_type == "llm-query":
-        backprompt_query = f"Using exactly one (no more, no fewer) of each of the numbers {instance_text} and the basic arithmetic operations (+ - * /), it is possible to write an expression that evaluates to 24."
-        backprompt_query+= f"Please check if the following expression uses exactly one of each number and evaluates to 24:" +model_response
+    if backprompt_type == "llm-query":
+        backprompt_query = f"Using exactly one (no more, no fewer) of each of the numbers {instance_text} and the basic arithmetic operations (+ - * /), it is possible to write an expression that evaluates to 24. "
+        backprompt_query+= f"Please check if the following expression uses exactly one of each number and evaluates to 24: " +model_response
         backprompt_query+= f"\nIf it is, say '{STOP_PHRASE}' Do not provide anything else in your response. If it is not correct, please give feedback on what is wrong and how to correct it."
         return backprompt_query
     elif backprompt_type == "llm-wrapper":
@@ -89,6 +97,12 @@ def backprompt(instance_text, model_response, backprompt_type):
         backprompt += model_response
         backprompt += f"\n\nWith this feedback, please try again. {DEFAULT_BACKPROMPT_END(instance_text)}"
         return backprompt
+    elif check:
+        return STOP_PHRASE
+    if backprompt_type == "passfail":
+        return f"Feedback: This is not correct. {DEFAULT_BACKPROMPT_END(instance_text)}"
+    elif backprompt_type == "first":
+        return f"Feedback: This is not correct. {reason} {DEFAULT_BACKPROMPT_END(instance_text)}"
     else: raise NotImplementedError
 
 #### Instance conversion script
