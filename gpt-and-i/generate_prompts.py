478f8b173218dcb7025e605d368c5a6512b8fb62
kstechly <kstechly@gmail.com>
2024-09-22 12:16:10 -0700

    refactoring

--- gpt-and-i/generate_prompts.py
+++ gpt-and-i/generate_prompts.py
@@ -4,13 +4,6 @@ import utils
 import domain_utils
 from fire import Fire #type: ignore
 
-def read_instance(domain_name,number_of_instance,file_ending):
-    try:
-        with open(f"data/instances/{domain_name}/instance-{number_of_instance}{file_ending}") as fp:
-            return fp.read()
-    except FileNotFoundError:
-        print(f"data/instances/{domain_name}/instance-{number_of_instance} not found.")
-
 def generate_prompts(domain_name, problem_type="", start=0, end=0, overwrite_previous=False):
     prompts = utils.read_json(domain_name, overwrite_previous, "instances")
     dmn = domain_utils.get_domain(domain_name)
@@ -24,10 +17,10 @@ def generate_prompts(domain_name, problem_type="", start=0, end=0, overwrite_pre
         if not overwrite_previous and str(x) in prompts.keys():
             print(f"Skipping instance-{x}, as it already exists.")
             continue
-        instance = read_instance(domain_name,x,dmn.file_ending())
+        instance = utils.read_instance(domain_name,x,dmn.file_ending())
         if instance: prompts[f"{x}"] = dmn.generate(instance, problem_type)
     
-    utils.write_json(domain_name, prompts, "instances")
+    utils.write_json(domain_name, prompts, "prompts")
 
 if __name__=="__main__":
     Fire(generate_prompts)
