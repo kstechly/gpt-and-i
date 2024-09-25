fed9f545c4232eee3da231396364ca7f73233513
kstechly <kstechly@gmail.com>
2024-09-25 10:39:11 -0700

    updating query scripts and g24 to new, clean, parallelized format

--- gpt-and-i/query_llm.py
+++ gpt-and-i/query_llm.py
@@ -1,6 +1,6 @@
 from fire import Fire
 from openai import OpenAI
-from concurrent.futures import ProcessPoolExecutor, as_completed, wait, FIRST_COMPLETED
+from concurrent.futures import ThreadPoolExecutor, as_completed, wait, FIRST_COMPLETED
 import domain_utils
 import utils
 from rich.progress import Progress, SpinnerColumn, TextColumn, BarColumn, TaskProgressColumn, TimeRemainingColumn
@@ -12,7 +12,7 @@ MAX_GPT_RESPONSE_LENGTH_SAFE = 300 #something to restrict output to if everythin
 MAX_BACKPROMPT_LENGTH = 15
 STOP_PHRASE = "stop10002" # "Verifier confirmed success" # what the verifier has to say
 STOP_STATEMENT = "[ANSWER END]" # what we look for to end LLM response generation
-MAX_WORKERS = 100
+MAX_WORKERS = 1000
 
 input_costs_per_million  = {
     "gpt-4": 30,
@@ -63,21 +63,21 @@ def process_instance(instance, domain_name, verbose=False):
     # Note that we assume that instance is already sorted by prompt_num
     # and that it isn't stopped, nor has it reached the maximum number of iterations
     if not instance[-1]["response"]:
-        if verbose: print(f">>Instance {instance[-1]['problem_id']} has never been seen before.==")
+        # if verbose: print(f">>Instance {instance[-1]['problem_id']} has never been seen before.==")
         prompt = instance[-1]["prompt"]
     else:
-        if verbose: print(f">>Generating backprompt {instance[-1]['prompt_num']+1} for instance {instance[-1]['problem_id']}.==")
+        # if verbose: print(f">>Generating backprompt {instance[-1]['prompt_num']+1} for instance {instance[-1]['problem_id']}.==")
         prompt = dmn.backprompt(instance)
         if check_backprompt(prompt): #TODO get rid of this part
             instance[-1]["stopped"] = True
             return instance
     llm = instance[-1]["llm"]
     temp = instance[-1]["temp"]
-    if verbose: print(f">>Instance {instance[-1]['problem_id']} is being processed with prompt:\n{prompt}")
+    # if verbose: print(f">>Instance {instance[-1]['problem_id']} is being processed with prompt:\n{prompt}")
     response, response_dict = send_query(prompt, llm, temp=temp)
     if not response: return None
     response_text = response.choices[0].message.content
-    if verbose: print(f">>Response to instance {instance[-1]['problem_id']}:\n{response_text}")
+    # if verbose: print(f">>Response to instance {instance[-1]['problem_id']}:\n{response_text}")
     if not instance[-1]["response"]:
         instance[-1]["response"] = response_text
         instance[-1]["response_object"] = response_dict
@@ -123,7 +123,7 @@ def get_responses(llm, domain_name, start=0, end=0, overwrite_previous=False, ve
     input = prepare_input(prompts, previous_output, llm, multiprompting, temp, trial_id, num_iterations)
     print(f">>Instances to be processed: {len(input)}")
 
-    total_tasks = len(input)
+    total_tasks = len(input)*num_iterations
     with Progress(
         SpinnerColumn(),
         TextColumn("[progress.description]{task.description}"),
@@ -131,13 +131,14 @@ def get_responses(llm, domain_name, start=0, end=0, overwrite_previous=False, ve
         TaskProgressColumn(),
         TimeRemainingColumn(),
         ) as progress:
-        task = progress.add_task("[cyan]Processing... ($0.00 cost)", total=total_tasks)
+        task = progress.add_task("[cyan]Processing... $0.00 (0 tasks in queue. 0 tasks completed)", total=total_tasks)
 
         failed = 0
         cost = 0
         stopped = 0
         maxed_out = 0
-        with ProcessPoolExecutor(max_workers=MAX_WORKERS) as executor:
+        completed_tasks = 0
+        with ThreadPoolExecutor(max_workers=MAX_WORKERS) as executor:
             print(f">>Deploying {min(MAX_WORKERS, total_tasks)} undergrads to process {total_tasks} instances.")
             futures = {executor.submit(process_instance, instance, domain_name, verbose) for instance in input}
             while futures and cost < max_cost:
@@ -146,24 +147,23 @@ def get_responses(llm, domain_name, start=0, end=0, overwrite_previous=False, ve
                     futures.remove(future)
                     new_instance = future.result()
                     if not new_instance: 
-                        progress.update(task, advance=1)
                         failed += 1
                         continue
                     else:
                         utils.write_jsonl(domain_name, new_instance[-1], "responses", llm=llm)
-                        if verbose: print(f">>Instance {new_instance[-1]['problem_id']} advanced successfully.")
+                        if verbose: print(f">>Instance {new_instance[-1]['problem_id']} advanced successfully to iteration {new_instance[-1]['prompt_num']}.")
                         cost+=new_instance[-1]["response_object"]["usage"]["prompt_tokens"]*input_costs_per_token[llm] + new_instance[-1]["response_object"]["usage"]["completion_tokens"]*output_costs_per_token[llm]
-                        progress.update(task, description=f"Processing... (${cost:.2f} cost)")
+                        completed_tasks += 1
                     if len(new_instance) >= num_iterations:
                         maxed_out +=1
                         if verbose: print(f">>Instance {new_instance[-1]['problem_id']} reached maximum iterations ({len(new_instance)}).")
-                        progress.update(task, advance=1)
                         continue
                     elif new_instance[-1]["stopped"]:
                         stopped +=1
                         if verbose: print(f">>Instance {new_instance[-1]['problem_id']} stopped successfully.")
-                        progress.update(task, advance=1)
+                        progress.update(task, advance=num_iterations-len(new_instance), description=f"Processing... ${cost:.2f} ({len(futures)} tasks in queue. {completed_tasks} tasks completed)")
                         continue
+                    progress.update(task, advance=1, description=f"Processing... ${cost:.2f} ({len(futures)} tasks in queue. {completed_tasks} tasks completed)")
                     futures.add(executor.submit(process_instance, new_instance, domain_name, verbose))
     print(f">>Complete. Total cost: ${cost}\n>>Number of failed instances: {failed}\n>>Number of stopped instances: {stopped}\n>>Number of maxed out instances (at {num_iterations} iterations): {maxed_out}")
 
